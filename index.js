b0c0f45fac33f9085dda4f70836a4a5715103e0d
Daniel Santos <danielsantos.ee@gmail.com>
2025-01-28 10:16:13 -0300

    primeiros passos

---
+++ index.js
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <div>
+      <h1>É O TEJOSBCINHA N TEM JEITO</h1>
+      <img
+        src="https://imgur.com/OIwJ1nU.jpg"
+        alt="kbcinha 2/16"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+      <img
+        src="https://i.imgur.com/JpRoN1K.png"
+        alt="kbcinha 5/15"
+        style={{ maxWidth: "100%", height: "auto" }}
+      />
+    </div>
+  );
+}
+
+export default Home;
